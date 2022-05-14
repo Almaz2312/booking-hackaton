8124478fb0e2e592f8a0748934c9703f201c4353
Almaz2312 <Almazdurusaliev@gmail.com>
2022-05-14 10:23:08 +0600

    added filter by conference parameter to list view

--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
 
 from booking.models import ConferenceBooking
 from booking.paginators import BookingPagination
@@ -10,14 +11,22 @@ class ConferenceBookingListAPIView(generics.ListAPIView):
     queryset = ConferenceBooking.objects.all()
     pagination_class = BookingPagination
 
+    def get_queryset(self):
+        queryset = ConferenceBooking.objects.all()
+        conference = self.request.query_params.get('conference')
+        if conference:
+            queryset = queryset.filter(
+                conference=conference,
+            )
+        return queryset
+
 
 class ConferenceBookingCreateAPIView(generics.CreateAPIView):
-    queryset = ConferenceBooking
+    queryset = ConferenceBooking.objects.all()
     serializer_class = ConferenceBookingSerializer
 
 
 class ConferenceBookingDetailUpdateDeleteAPIView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = ConferenceBookingSerializer
-    queryset = ConferenceBooking
-
+    queryset = ConferenceBooking.objects.all()
 
